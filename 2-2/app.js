889eae64b9349ef5a9d8c01f54dd5f712e2e0669
Yoon-kiyeong <kyyoon2105@naver.com>
2023-05-18 19:17:12 +0900

    2-2 Painting
    2023-05-18
    아직 다 안함

---
+++ 2-2/app.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = 2;
+
+ctx.moveTo(200, 200);
+
+ctx.lineTo(400, 400);
+ctx.stroke();
